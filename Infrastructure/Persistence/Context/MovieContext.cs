0810a228af4def28c3fdc2b01ca519ada77679b3
mehmetalbayrak <mehmet__albayrak@outlook.com>
2025-09-23 17:30:49 +0300

    created entities

---
+++ Infrastructure/Persistence/Context/MovieContext.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Context
+{
+    public class MovieContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-JDMU1LH;initial Catalog=MovieApiDb;integrated Security=true;");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Cast> Casts { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
