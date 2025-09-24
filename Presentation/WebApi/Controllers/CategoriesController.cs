b5d0c0412783405d4a233b1f367d896ab3f4290d
mehmetalbayrak <mehmet__albayrak@outlook.com>
2025-09-24 20:09:13 +0300

    worked api

---
+++ Presentation/WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,57 @@
+﻿using Application.Features.CQRS.Commands.Category;
+using Application.Features.CQRS.Handlers.Category;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly GetCategoryQueryHandler _getCategoryQueryHandler;
+        private readonly GetCategoryByIdQueryHandler _getCategoryByIdQueryHandler;
+        private readonly CreateCategoryCommandHandler _createCategoryCommandHandler;
+        private readonly UpdateCategoryCommandHandler _updateCategoryCommandHandler;
+        private readonly RemoveCategoryCommandHandler _removeCategoryCommandHandler;
+
+        public CategoriesController(GetCategoryQueryHandler getCategoryQueryHandler, GetCategoryByIdQueryHandler getCategoryByIdQueryHandler, CreateCategoryCommandHandler createCategoryCommandHandler, RemoveCategoryCommandHandler removeCategoryCommandHandler)
+        {
+            _getCategoryQueryHandler = getCategoryQueryHandler;
+            _getCategoryByIdQueryHandler = getCategoryByIdQueryHandler;
+            _createCategoryCommandHandler = createCategoryCommandHandler;
+            _removeCategoryCommandHandler = removeCategoryCommandHandler;
+        }
+        [HttpGet]
+        public async Task<IActionResult> CategoryList()
+        {
+            var value = await _getCategoryQueryHandler.Handle();
+            return Ok(value);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryCommand command)
+        {
+           await _createCategoryCommandHandler.Handle(command);
+            return Ok("Kategori ekleme işlemi başarılı.");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteCategory(int id)
+        {
+            await _removeCategoryCommandHandler.Handle(new RemoveCategoryCommand(id));
+            return Ok("Silme işlemi başarılı.");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateCategory(UpdateCategoryCommand command)
+        {
+            await _updateCategoryCommandHandler.Handle(command);
+            return Ok("Güncelleme işlemi başarılı.");
+        }
+        [HttpGet("GetCategory")]
+        public async Task<IActionResult> GetCategory(int id)
+        {
+            var value = await _getCategoryByIdQueryHandler.Handle(new Application.Features.CQRS.Queries.Category.GetCategoryByIdQuery(id));
+            return Ok(value);
+        }
+    }
+}
--- Presentation/WebApi/Program.cs
+++ Presentation/WebApi/Program.cs
@@ -1,6 +1,24 @@
+using Application.Features.CQRS.Handlers.Category;
+using Application.Features.CQRS.Handlers.Movie;
+using Persistence.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<MovieContext>();
+
+builder.Services.AddScoped<GetCategoryQueryHandler>();
+builder.Services.AddScoped<GetCategoryByIdQueryHandler>();
+builder.Services.AddScoped<CreateCategoryCommandHandler>();
+builder.Services.AddScoped<RemoveCategoryCommandHandler>();
+builder.Services.AddScoped<UpdateCategoryCommandHandler>();
+
+builder.Services.AddScoped<GetMovieQueryHandler>();
+builder.Services.AddScoped<GetMovieByIdQueryHandler>();
+builder.Services.AddScoped<CreateMovieCommandHandler>();
+builder.Services.AddScoped<RemoveMovieCommandHandler>();
+builder.Services.AddScoped<UpdateMovieCommandHandler>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
